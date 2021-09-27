3b53551c485c2ee32514070e292bbf104ed47b27
Mikhail Prokudin <mirex.doors@mail.ru>
2021-09-27 16:43:57 +0700

    add chaining to promise polyfill

--- promise.js
+++ promise.js
@@ -1,54 +1,80 @@
-const statuses = {
+const states = {
     pending: 'PENDING',
-    fullfilled: 'FULLFILLED',
+    fulfilled: 'fulfilled',
     rejected: 'REJECTED',
 }
 
 class MyPromise {
     #state;
-    #thenFn = () => {
-    };
-    #catchFn = () => {
-    };
+    #deferred = [];
+    #value;
 
     constructor(fn) {
-        this.#state = statuses.pending;
-        return fn(this.#resolve.bind(this), this.#reject.bind(this));
+        if (typeof fn !== 'function') {
+            throw new TypeError("not a function");
+        }
+        this.#state = states.pending;
+
+        try {
+            fn(this.#resolve.bind(this), this.#reject.bind(this));
+        } catch (e) {
+            this.#reject.bind(this)(e);
+        }
     }
 
     #resolve(data) {
-        if (this.#state === statuses.pending) {
-            this.#state = statuses.fullfilled;
-            setTimeout(() => {
-                try {
-                    this.#thenFn(data);
-                } catch (e) {
-                    this.#catchFn(e);
-                    this.#state = statuses.rejected;
-                }
-            }, 0)
+        if (this.#state === states.pending) {
+            this.#state = states.fulfilled;
+            this.#value = data;
+            this.#handle();
         }
     }
 
     #reject(error) {
-        if (this.#state === statuses.pending) {
-            this.#state = statuses.rejected;
-            setTimeout(() => {
-                return this.#catchFn(error);
-            }, 0);
+        if (this.#state === states.pending) {
+            this.#state = states.rejected;
+            this.#value = error;
+            this.#handle();
         }
     }
 
+    #handle() {
+        if (this.#state === states.rejected && this.#deferred.length === 0) {
+            console.log("Unhandled promise rejection", this.#value)
+        }
+
+        this.#deferred.forEach((deferred) => {
+            setTimeout(() => {
+                const callback = this.#state === states.fulfilled ? deferred.onResolved : deferred.onRejected;
+                if (callback === null) {
+                    if (this.#state === states.fulfilled) {
+                        this.#resolve.bind(deferred.promise)(this.#value);
+                    } else {
+                        this.#reject.bind(deferred.promise)(this.#value);
+                    }
+                    return;
+                }
+
+                let result;
+
+                try {
+                    result = callback(this.#value);
+                } catch (e) {
+                    this.#reject.bind(deferred.promise)(e);
+                }
+                this.#resolve.bind(deferred.promise)(result);
+            }, 0);
+        });
+    }
+
     then(onResolved, onRejected) {
-        if (onResolved) {
-            this.#thenFn = onResolved;
-        }
-
-        if (onRejected) {
-            this.#catchFn = onRejected;
-        }
-
-        return this;
+        const promise = new this.constructor(() => {});
+        this.#deferred.push({
+            onResolved: typeof onResolved === 'function' ? onResolved : null,
+            onRejected: typeof onRejected === 'function' ? onRejected : null,
+            promise
+        });
+        return promise;
     }
 
     catch(onRejected) {
@@ -59,7 +85,16 @@ class MyPromise {
 const promiseTimeout = new MyPromise((resolve, reject) => {
     setTimeout(() => {
         resolve('Time is over');
+        reject(new Error('Error'))
     }, 1000);
 });
 
-promiseTimeout.then((data) => console.log(data)).catch(err => console.error(err))
+promiseTimeout
+    .then((data) => {
+        console.log(data)
+        throw new Error("Error!")
+    })
+    .then(() => {
+        console.log("step2")
+    })
+    .catch(err => console.error(err))
